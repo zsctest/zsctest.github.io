4307b589ebe82ba09ba6e578d4bb861cb38b0d2b
zsctest <zscjayden@qq.com>
2026-06-15 01:36:36 +0800

    test

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,7 +2,7 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://astro-paper.pages.dev/",
+    url: "https://zsctest.github.io",
     title: "AstroPaper",
     description: "A minimal, responsive and SEO-friendly Astro blog theme.",
     author: "Sat Naing",
